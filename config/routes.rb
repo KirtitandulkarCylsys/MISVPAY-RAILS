55d76733628f0b709da5d918333c41e1ed6768b0
Kirtitandulkar362000 <115093565+Kirtitandulkar362000@users.noreply.github.com>
2023-09-29 12:11:50 +0530

    rails new project

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      # resources :login_details
+      resources :summary_transactions
+      # resources :scheme_details 
+      # resources :region_summary_transactions
+      # resources :ufc_summary_transactions
+      # resources :rm_summary_transactions
+      # resources :aum
+      # resources :aum_regions
+      # resources :aum_ufc
+      # resources :aum_dropdown
+      # resources :all_region_retail
+      # resources :all_ufc_retail
+      # resources :all_rm_retail
+      # resources :assetclass
+    end
+  end
+end
