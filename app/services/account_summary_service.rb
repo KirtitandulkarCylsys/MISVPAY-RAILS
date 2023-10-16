e592878bb982f1a51d844ccdaa76919c6946955c
SonuKr14 <140049897+SonuKr14@users.noreply.github.com>
2023-10-16 18:50:23 +0530

    ShowData

--- app/services/account_summary_service.rb
+++ app/services/account_summary_service.rb
@@ -1,29 +1,101 @@
 require 'oci8'
-
 class AccountSummaryService
-    def self.get_account_summary_report_rm(employee_id,employee_role,
-      quarter,start_date, end_date, amount, condition, transaction_type, ufc,zone,region_code,ufc_code,rmcode,no_mapping,channel_code,common_report)
+    def self.get_account_summary_report_rm(
+      employee_id,
+      employee_role,
+      quarter,
+      account_no,
+      pan_no,
+      pin_code,
+      arn_no,
+      subARN_no,
+      ufc,
+      amount,
+      condition,
+      karvy_location,
+      start_date,
+      end_date, 
+      scheme_code,
+      ria_code,
+      trans_RM_code,
+      ihno,
+      scheme_type,
+      euin,
+      transaction_type,
+      bsf,
+      no_mapping,
+      zone,
+      region_code,
+      ufc_code,
+      rmcode,
+      channel_code,
+      common_report)
       conn = OCI8.new('MISVPAY', 'MISVPAY@123', '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=103.12.1.155)(PORT=1521))(CONNECT_DATA=(SID=xe)))')
       procedure_name = 'amount_report_sales'
-      cursor = conn.parse("BEGIN #{procedure_name}(:P_EMPLID, :p_emprole,:p_quarter, :p_startDate,:p_endDate,:p_amount,:P_condition,:p_transation_type, :p_ufc,:p_zone,:P_REGION_CODE,:p_ufc_code,:P_RMCODE,:p_no_mapping,:P_CHN_Code,:P_COMMON_REPORT, :get_all_data); END;")
+      cursor = conn.parse("BEGIN #{procedure_name}(
+        :P_EMPLID,  
+        :P_EMPROLE,  
+        :P_QUARTER,  
+        :P_ACCOUNT_NO,  
+        :P_PAN_NO,  
+        :P_PIN_CODE,  
+        :P_ARN_NO,  
+        :P_SUBARN_NO, 
+        :P_UFC,  
+        :P_AMOUNT,  
+        :P_CONDITION,  
+        :P_KARVY_LOCATION,  
+        :P_STARTDATE, 
+        :P_ENDDATE,  
+        :P_SCHEME_CODE,  
+        :P_RIA_CODE,  
+        :P_TRANS_RM_CODE,  
+        :P_IHNO,  
+        :P_SCHEME_TYPE,  
+        :P_EUIN,  
+        :P_TRANSATION_TYPE,  
+        :P_BSF ,
+        :P_NO_MAPPING,  
+        :P_ZONE,  
+        :P_REGION_CODE,  
+        :P_UFC_CODE,  
+        :P_RMCODE,  
+        :P_CHN_CODE,
+        :P_COMMON_REPORT, 
+        :get_all_data); END;")
       cursor.bind_param(':P_EMPLID',employee_id, String)
-      cursor.bind_param(':p_emprole',employee_role, String)
-      cursor.bind_param(':p_quarter',quarter, String)
-      cursor.bind_param(':p_startDate', start_date, String)
-      cursor.bind_param(':p_endDate', end_date, String)
-      cursor.bind_param(':p_amount', amount, String)
-      cursor.bind_param(':P_condition', condition, String)
-      cursor.bind_param(':p_transation_type', transaction_type, String)
-      cursor.bind_param(':p_ufc',ufc, String)
-      cursor.bind_param(':p_zone',zone, String)
+      cursor.bind_param(':P_EMPROLE',employee_role, String)
+      cursor.bind_param(':P_QUARTER',quarter, String)
+      cursor.bind_param(':P_ACCOUNT_NO',account_no, String)
+      cursor.bind_param(':P_PAN_NO',pan_no, String)
+      cursor.bind_param(':P_PIN_CODE',pin_code, String)
+      cursor.bind_param(':P_ARN_NO',arn_no, String)
+      cursor.bind_param(':P_SUBARN_NO',subARN_no, String)
+      cursor.bind_param(':P_UFC',ufc, String)
+      cursor.bind_param(':P_AMOUNT', amount, String)
+      cursor.bind_param(':P_CONDITION', condition, String)
+      cursor.bind_param(':P_KARVY_LOCATION',karvy_location, String)
+      cursor.bind_param(':P_STARTDATE', start_date, String)
+      cursor.bind_param(':P_ENDDATE', end_date, String)
+      cursor.bind_param(':P_SCHEME_CODE',scheme_code, String)
+      cursor.bind_param(':P_RIA_CODE', ria_code, String)
+      cursor.bind_param(':P_TRANS_RM_CODE', trans_RM_code, String)
+      cursor.bind_param(':P_IHNO', ihno, String)
+      cursor.bind_param(':P_SCHEME_TYPE', scheme_type, String)
+      cursor.bind_param(':P_EUIN', euin, String)
+      cursor.bind_param(':P_TRANSATION_TYPE', transaction_type, String)
+      cursor.bind_param(':P_BSF', bsf, String)
+      cursor.bind_param(':P_NO_MAPPING',no_mapping, String)
+      cursor.bind_param(':P_ZONE',zone, String)
       cursor.bind_param(':P_REGION_CODE',region_code, String)
-      cursor.bind_param(':p_ufc_code',ufc_code, String)
+      cursor.bind_param(':P_UFC_CODE',ufc_code, String)
       cursor.bind_param(':P_RMCODE',rmcode, String)
-      cursor.bind_param(':p_no_mapping',no_mapping, String)
-      cursor.bind_param(':P_CHN_Code',channel_code, String)
+      cursor.bind_param(':P_CHN_CODE',channel_code, String)
       cursor.bind_param(':P_COMMON_REPORT',common_report, String)
       cursor.bind_param(':get_all_data', nil, OCI8::Cursor)    
+    
       cursor.exec
+      
       sales_report = []
       out_cursor = cursor[':get_all_data']
       columns = out_cursor.column_metadata
